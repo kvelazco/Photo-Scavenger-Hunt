96b9ee906c90790653408a94a6eb73f7087e67d1
Kevin Velazco <kvela033@fiu.edu>
2023-02-19 17:18:15 -0500

    Initial Commit

---
+++ Photo Scavenger Hunt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photo Scavenger Hunt
+//
+//  Created by Kevin Velazco on 2/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
